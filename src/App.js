1e9fe28cf22dc438902b55305cb759511713d11f
Anisa Majmari <anisamajmari@gmail.com>
2023-10-31 10:31:42 +0100

    header elements added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import Login from "./components/Login";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -9,6 +11,14 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />}></Route>
+          <Route
+            path="/home"
+            element={
+              <>
+                <Header /> <Home />
+              </>
+            }
+          ></Route>
         </Routes>
       </BrowserRouter>
     </div>
