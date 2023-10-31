306ac63fbbbace07f9e16c288bcdde62a4829655
Anisa Majmari <anisamajmari@gmail.com>
2023-10-31 09:48:23 +0100

    sign in with Google button added

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,6 +17,13 @@ const Login = (props) => {
           <h1> Welcome to your professional community</h1>
           <img src="/images/login-hero.svg" alt="" />
         </Hero>
+
+        <Form>
+          <Google>
+            <img src="/images/google.svg" alt="" />
+            Sign in with Google
+          </Google>
+        </Form>
       </Section>
     </Container>
   );
@@ -109,7 +116,7 @@ const Hero = styled.div`
     color: #2977c9;
     font-weight: 200;
     line-height: 70px;
-    @media (max-width: 768) {
+    @media (max-width: 768px) {
       text-align: center;
       font-size: 20px;
       width: 100%;
@@ -119,7 +126,7 @@ const Hero = styled.div`
 
   img {
     /* z-index: -1; */
-    width: 700px;
+    width: 800px;
     height: 670px;
     position: absolute;
     bottom: -2px;
@@ -131,6 +138,37 @@ const Hero = styled.div`
       position: initial;
       height: initial;
     }
+  }
+`;
+
+const Form = styled.div`
+  margin-top: 100px;
+  width: 408px;
+
+  @media (max-width: 768px) {
+    margin-top: 20px;
+  }
+`;
+
+const Google = styled.button`
+  display: flex;
+  justify-content: center;
+  background-color: #fff;
+  align-items: center;
+  height: 56px;
+  width: 100%;
+  border-radius: 28px;
+  box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%),
+    inset 0 0 0 2px rgb(0 0 0 / 0%) inset 0 0 0 1px rgb(0 0 0 / 0%);
+  vertical-align: middle;
+  z-index: 0;
+  transition-duration: 167ms;
+  font-size: 20px;
+  color: rgba(0, 0, 0, 0.6);
+
+  &:hover {
+    background-color: rgba(207, 207, 207, 0.25);
+    color: rgba(0, 0, 0, 0.75);
   }
 `;
 
